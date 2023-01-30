d2beca1a6040416fce16edc9a60f115151afff94
Joseph DeCommer <jdecommer@gmail.com>
2023-01-30 09:26:57 -0500

    added comments and cleanup

--- StringReverse.java
+++ StringReverse.java
@@ -1,43 +1,41 @@
-import java.util.Arrays;
-import java.util.stream.Collectors;
+//import java.util.Arrays;
+//import java.util.stream.Collectors;
 
 public class StringReverse {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         String string = "I evol ym !ecnatsnoC";
         System.out.println(reverseWords(string));
     }
-    public static String reverseStr(String input) {
 
+    public static String reverseStr(String input)
+    {
         if(input.equals("")) {
             return "";
         }
         return reverseStr(input.substring(1)) + input.charAt(0);
     }
 
-
-    public static String reverseStrInPlace(String input) {
+/*     public static String reverseStrInPlace(String input) //function not used
+    { 
         String reversed = Arrays.stream(input.split(" "))
         .map(word -> new StringBuilder(word).reverse())
         .collect(Collectors.joining(" "));
         
         return reversed;
-    }
+    } */
 
-
-    public static String reverseWords(final String original)
+    public static String reverseWords(final String original) //function
     { 
       String[] array = original.split(" ");
-      
       if(array.length == 0) 
           return original;
-      
-      
       int i = 0;
-      for(String string : array){
+      for(String string : array)
+      {
           array[i] = new StringBuilder(string).reverse().toString();
           i++;
       }
-      
       return String.join(" ",array);
     }
 }
