1fd8e8cb50c29f35c667556732b5bceb2d868868
Joseph DeCommer <jdecommer@gmail.com>
2023-02-18 19:37:36 -0500

    replaced arrayDiff function

--- ArrayDiff.java
+++ ArrayDiff.java
@@ -1,49 +1,18 @@
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ArrayDiff {
     public static void main(String[] blergs) {
-
         int[] one = {2, 2};
-        int[] two = {1, 2, 2};
-
+        int[] two = {1, 2};
         System.out.println(Arrays.toString(arrayDiff(one, two)));
-
     }
 
-public static int[] arrayDiff(int[] a, int[] b) {
-        int[] result;
-
-        int size = a.length;
-
-        if (a.length == 0 || b.length == 0) {
-            return a;
-        } else {
-            for (int i = 0; i < a.length; i++) {
-                for (int j = 0; j < b.length; j++) {
-                    if (a[i] == b[j]) {
-                        size--;
-                        break;
-                    }
-                }
-            }
-            result = new int[size];
-            
-            int index = 0;
-            int countEq = 0;
-            for (int i = 0; i < a.length; i++) {
-                countEq = 0;
-                for (int j = 0; j < b.length; j++) {
-                    if (a[i] == b[j]) {
-                        countEq++;
-                        break; 
-                    }
-                }
-                if (countEq == 0) {
-                    result[index] = a[i];
-                    index++;
-                }
-            }
-        }
-        return result;
+    public static int[] arrayDiff(int[] a, int[] b) {
+        List<Integer> listA = Arrays.stream(a).boxed().collect(Collectors.toList());
+        List<Integer> listB = Arrays.stream(b).boxed().collect(Collectors.toList());
+        listA.removeAll(listB);
+        return listA.stream().mapToInt(e -> e).toArray();
     }
 }
