1159d5be8e3feae92cdac1ebb31401640e421c6b
Joseph DeCommer <jdecommer@gmail.com>
2024-01-06 17:23:20 -0500

    new file

---
+++ Beginagain.java
@@ -0,0 +1,12 @@
+
+public class Beginagain{
+    public static void main(String[] args){
+
+        int num = 2;
+
+        ++num;
+        num++;
+        System.out.println(num);
+        
+    }
+}
