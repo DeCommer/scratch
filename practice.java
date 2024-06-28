38189a5e54886cff17cbda580f3797ce80c480bb
Joseph DeCommer <jdecommer@gmail.com>
2024-06-28 13:08:15 -0400

    java pratice

--- practice.java
+++ practice.java
@@ -2,32 +2,22 @@
 public class practice {
 
     public static void main(String[] args) {
-        
-        // int target = 14;
-        // int[] testArr= {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-        // for(int i = 0;  i < testArr.length; i++) {
-        //     if(testArr[i] == target){
-        //         System.out.println(testArr[i]);
-        //     }
-        // }
-        fizzyBuzz(10);
+
+        System.out.println(sumNums(10));
+        System.out.println(sumNums1(10));
+
     }
 
+    public static int sumNums1(int n) {
+        return n * (n + 1) / 2;
+    }
 
-        //FizzBuzz Practice
-
-        public static int fizzyBuzz(int n) {
-            for(int i = 0; i <= n; i++) {
-                if(i % 3 == 0 && i % 5 == 0) {
-                    System.out.println(i + " Multiple of 3 and 5");
-                } else if (i % 3 == 0) {
-                    System.out.println(i + " Multiple of only 3");
-                } else if (i % 5 == 0) {
-                    System.out.println(i+ " Multiple of only 5");
-                }else {
-                    System.out.println(i + " Mutiple of nither 3 nor 5");
-                }
-            }
-            return n;
+    public static int sumNums(int n) {
+        int sum = 0;
+        for(int i = 1; i <= n; i++) {
+            sum += i;
         }
+        return sum;
+    }
+
 }
